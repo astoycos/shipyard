astoycos/shipyard

commit a1ab011f2271478a694aeba864099f09f37ff93d

Expose settings as `make` variable
This way it can be accessed conveniently by all scripts.

Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

Makefile
@@ -13,17 +13,15 @@ ifneq (,$(DAPPER_HOST_ARCH))
 
 # Running in Dapper
 
-include Makefile.inc
-
 ifneq (,$(filter ovn,$(_using)))
-CLUSTER_SETTINGS_FLAG = --settings $(DAPPER_SOURCE)/.shipyard.e2e.ovn.yml
+SETTINGS ?= $(DAPPER_SOURCE)/.shipyard.e2e.ovn.yml
 else
-CLUSTER_SETTINGS_FLAG = --settings $(DAPPER_SOURCE)/.shipyard.e2e.yml
+SETTINGS ?= $(DAPPER_SOURCE)/.shipyard.e2e.yml
 endif
 
-override CLUSTERS_ARGS += $(CLUSTER_SETTINGS_FLAG)
-override DEPLOY_ARGS += $(CLUSTER_SETTINGS_FLAG)
 override E2E_ARGS += --nolazy_deploy cluster1
+
+include Makefile.inc
 
 # Prevent rebuilding images inside dapper since thy're already built outside it in Shipyard's case
 package/.image.nettest package/.image.shipyard-dapper-base: ;

Makefile.inc
@@ -78,6 +78,12 @@ override DEPLOY_ARGS += --plugin $(PLUGIN)
 override CLEANUP_ARGS += --plugin $(PLUGIN)
 endif
 
+ifdef SETTINGS
+export SETTINGS
+override CLUSTERS_ARGS += --settings $(SETTINGS)
+override DEPLOY_ARGS += --settings $(SETTINGS)
+endif
+
 GO ?= go
 export GO
 

commit 1f61657db6a8f8d3dca0cd9479924b0585e28b50

Fix `make clusters using=ovn`
a1ab011f2271478a694aeba864099f09f37ff93d broke
being able to specify a cable driver because
the setting were populated before the `using` var
was parsed from the command line

This commit ensures we parse the `using=` flag first
thing

Signed-off-by: Andrew Stoycos <astoycos@redhat.com>

Makefile
@@ -7,6 +7,8 @@ FOCUS ?=
 SKIP ?=
 PLUGIN ?=
 
+_using = $(subst $(,), ,$(using))
+
 export BASE_BRANCH OCM_BASE_BRANCH
 
 ifneq (,$(DAPPER_HOST_ARCH))

Makefile.inc
@@ -1,5 +1,4 @@
 , := ,
-_using = $(subst $(,), ,$(using))
 PRELOAD_IMAGES ?= submariner-gateway submariner-operator submariner-route-agent
 
 # Check Makefile.dapper freshness